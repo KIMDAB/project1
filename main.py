e23a8a1e08091d9b75cb4f2674ae652bc9cb5d48
KIMDAB <shiny_007@naver.com>
2024-07-02 14:30:33 +0900

    두 번째 커밋 뺴기 추가 구현

--- main.py
+++ main.py
@@ -2,11 +2,19 @@
 def add_func(n1, n2):
     retVal = n1 +n2
     return retVal
+def sub_func(n1, n2):
+    retVal = n1 -n2
+    return retVal
+
 ##변수부
 num1, num2, res = 100, 200, 0
 ##메인코드
 
 res = add_func(num1, num2)
 print(num1, '+', num2, '=', res)
+
+res2= sub_func(num1, num2)
+print(num1, '-', num2, '=', res)
+
 
 
